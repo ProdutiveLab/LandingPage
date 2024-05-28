47fdd0df83edd8dad12575c433ed5bf84dd59016
nelo <nelodvn@gmail.com>
2024-05-28 19:35:29 -0300

    removing comments

--- sst.config.ts
+++ sst.config.ts
@@ -12,7 +12,6 @@ export default $config({
   async run() {
 
     new sst.aws.Nextjs("CTLandingPage", {
-      // domain: "contatempo.com.br",
       domain: {
         name: $app.stage === "production" ? "contatempo.com.br" : undefined,
         aliases: $app.stage === "production" ? ["www.contatempo.com.br"] : undefined,
